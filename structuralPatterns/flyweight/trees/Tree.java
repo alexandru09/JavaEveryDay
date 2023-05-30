acad85455e6178f615d5edea42294ce6a4e57867
alexandru09 <tudor.alexandru1996@gmail.com>
2023-05-30 09:01:32 +0300

    Flyweight design pattern

---
+++ structuralPatterns/flyweight/trees/Tree.java
@@ -0,0 +1,24 @@
+package structuralPatterns.flyweight.trees;
+
+import java.awt.*;
+
+/*
+ * Unique state of each tree
+ * https://refactoring.guru/design-patterns/flyweight/java/example#example-0
+ */
+public class Tree {
+    private int x;
+    private int y;
+    private TreeType type;
+
+    public Tree(int x, int y, TreeType type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public void draw(Graphics g) {
+        type.draw(g, x, y);
+    }
+    
+}
---
+++ structuralPatterns/flyweight/trees/TreeFactory.java
@@ -0,0 +1,22 @@
+package structuralPatterns.flyweight.trees;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.awt.*;
+
+/*
+ * Creation of flyweight
+ * https://refactoring.guru/design-patterns/flyweight/java/example#example-0
+ */
+public class TreeFactory {
+    static Map<String, TreeType> treeTypes = new HashMap<>();
+
+    public static TreeType getTreeType(String name, Color color, String otherTreeData) {
+        TreeType result = treeTypes.get(name);
+        if (result == null) {
+            result = new TreeType(name, color, otherTreeData);
+            treeTypes.put(name, result);
+        }
+        return result;
+    }
+}
