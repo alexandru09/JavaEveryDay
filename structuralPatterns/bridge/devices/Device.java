c99d4745c2255f0780b4fc7c812c48401e69b671
alexandru09 <tudor.alexandru1996@gmail.com>
2023-06-03 18:40:41 +0300

    Bridge design pattern

---
+++ structuralPatterns/bridge/devices/Device.java
@@ -0,0 +1,22 @@
+package structuralPatterns.bridge.devices;
+
+/*
+ * Bridge pattern example from https://refactoring.guru/design-patterns/bridge/java/example
+ */
+public interface Device {
+    boolean isEnabled();
+
+    void enable();
+
+    void disable();
+
+    int getVolume();
+
+    void setVolume(int percent);
+
+    int getChannel();
+
+    void setChannel(int channel);
+
+    void printStatus();
+}
