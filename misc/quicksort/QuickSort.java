e72bdadf36ec873d46b3f8cefe105c170d1db20c
alexandru09 <tudor.alexandru1996@gmail.com>
2023-06-11 15:58:37 +0300

    Quicksort implementation

---
+++ misc/quicksort/QuickSort.java
@@ -0,0 +1,46 @@
+package misc.quicksort;
+
+import javax.swing.*;
+
+public class QuickSort {
+
+    private static int partition(int[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i+1];
+        arr[i+1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i + 1;
+    }
+
+    public static void quickSort(int[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 1, 6, 7, 3};
+
+        quickSort(array, 0, 4);
+
+        for (int a :
+                array) {
+            System.out.println(a + " ");
+        }
+    }
+}
